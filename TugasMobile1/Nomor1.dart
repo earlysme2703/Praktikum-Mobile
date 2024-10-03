a44261083436cd13cd8a530a35b50d7a4e28b4cc
earlysme2703 <earlymaulana16@gmail.com>
2024-10-03 23:01:16 +0700

    Luas lingkaran

---
+++ TugasMobile1/Nomor1.dart
@@ -0,0 +1,8 @@
+void main() {
+
+  double radius = 7.0;
+  double pi = 3.14;
+  double luasLingkaran = pi * radius * radius;
+
+  print("Luas lingkaran dengan jari-jari $radius adalah $luasLingkaran");
+}
